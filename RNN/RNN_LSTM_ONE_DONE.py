1bfd489685327d77e00d9f47e73fcf8ea4b9c63d
Tonmoy <tonmoyibnearif@gmail.com>
2018-10-08 19:36:43 +0200

    matplotlib and RMSE,MSE and MAE added

--- RNN/RNN_LSTM_ONE_DONE.py
+++ RNN/RNN_LSTM_ONE_DONE.py
@@ -9,6 +9,7 @@ from keras.layers import Dense
 from tensorflow.python.keras.optimizers import RMSprop
 from keras.layers import Dropout
 from keras.layers import LSTM
+import matplotlib.pyplot as plt
 
 
 dataset=pd.read_csv('Preprocessed_data_three.csv')
@@ -44,7 +45,7 @@ print(X_train.shape[0])
 
 #Creating 2 Hour Autoregression
 Testing_data=X_train.iloc[0:-2,0:6].values
-Target_data=Y_train.iloc[2:0,0:].values
+Target_data=Y_train.iloc[2:,:].values
 print(Testing_data.shape)
 
 Testing_data=np.array(Testing_data)
@@ -83,7 +84,33 @@ regressor.add(Dense(1))
 #compling the RNN
 regressor.compile(optimizer='adam',loss='mean_squared_error')
 #fitting the RNN to Training set
-regressor.fit(tmp_x_train,tmp_y_train,epochs=10,batch_size=100)
+regressor.fit(tmp_x_train,tmp_y_train,epochs=100,batch_size=100)
 
+Validation_data=X_test.reshape((-1,1,6))
+Predicted_windpower=regressor.predict(Validation_data)
+
+plt.figure(figsize=(15,10))
+plt.plot(Y_test, color='red',label='Test_Windpower_Data')
+plt.plot(Predicted_windpower, color='blue',label='Predicted_Windpower_Data')
+plt.title('Windfarm_Power_prediction')
+plt.xlabel('Time')
+plt.xlabel('Power')
+#plt.figure(figsize=(1,1))
+plt.legend()
+plt.show()
+
+from sklearn import metrics
+from sklearn.metrics import mean_squared_error
+#MSE
+MSE=(metrics.mean_squared_error(Y_test, Predicted_windpower))
+#MAE
+MAE=(metrics.mean_absolute_error(Y_test, Predicted_windpower))
+#RMSe
+RMSE=(np.sqrt(metrics.mean_squared_error(Y_test, Predicted_windpower)))
+#to print numpy array
+np.set_printoptions(precision=15)
+print ("MAE:%2f" %(MAE))
+print ("MSE:%2f" %(MSE))
+print ("RMSE:%2f" %(RMSE))
 
 
